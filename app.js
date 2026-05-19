e765ceebe6f177125cb8810a634fe6b9d66156bd
Feygon Nickerson <feygon@gmail.com>
2026-05-19 14:06:14 -0700

    Bump page version to 0.1.0

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let waitlist = normalizeWaitlist(JSON.parse(localStorage.getItem("sensorynav-wai
 
 localStorage.setItem("sensorynav-waitlist", JSON.stringify(waitlist, null, 2));
 
-fetch("build.json?v=0.0.9")
+fetch("build.json?v=0.1.0")
   .then((response) => response.json())
   .then((build) => {
     buildVersion.textContent = `v${build.version}`;
